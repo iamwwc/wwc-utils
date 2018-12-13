6478861f2b7cc2524870ed95dc5bf9a9bcac1670
iamwwc <qaq1362211689@gmail.com>
2018-12-13 09:50:48 +0800

    fix

--- src/merge.ts
+++ src/merge.ts
@@ -4,6 +4,6 @@
  * merge multiple objects, merge rules respect Object.assign
  * @param args 
  */
-export default function merge(...args: any) {
+export default function merge(...args: Array<any>) {
     return Object.assign({}, ...args)
 }
